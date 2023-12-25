f98f216538f43e963b0813e717dc3966b67eb7d5
sunsun1102 <sonhoang880@gmail.com>
2023-12-25 18:10:08 +0700

    25/12 model

---
+++ Models/DiemDanh.cs
@@ -0,0 +1,203 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CourseWebsiteDotNet.Models
+{
+    public class DiemDanhModel
+    {
+        public int id_hoc_vien { get; set; }
+        public int id_buoi_hoc { get; set; }
+        public string? ghi_chu { get; set; }
+        public int? co_mat {  get; set; }
+    }
+    public class DiemDanhRepository
+    {
+        private readonly string connectionString;
+        public DiemDanhRepository()
+        {
+            connectionString = DatabaseConnection.CONNECTION_STRING;
+        }
+
+        // Hàm try catch lỗi từ database
+        private Response ExecuteDatabaseOperation(Func<Response> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (MySqlException dbEx)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Database Exception: {dbEx.Message}",
+                    InsertedId = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Exception: {ex.Message}",
+                    InsertedId = null
+                };
+            }
+        }
+
+        // Trả về List<DiemDanhModel>
+        public List<DiemDanhModel> GetAllDiemDanh()
+        {
+            List<DiemDanhModel> diemDanhList = new List<DiemDanhModel>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM diem_danh";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            DiemDanhModel diemDanh = new DiemDanhModel();
+                            diemDanh.id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]);
+                            diemDanh.id_buoi_hoc = Convert.ToInt32(reader["id_buoi_hoc"]);
+                            diemDanh.ghi_chu = Convert.ToString(reader["ghi_chu"]);
+                            diemDanh.co_mat = Convert.ToInt32(reader["co_mat"]);
+                            diemDanhList.Add(diemDanh);
+                        }
+                    }
+                }
+            }
+
+            return diemDanhList;
+        }
+
+        // Trả về 1 DiemDanhModel
+        public DiemDanhModel? GetDiemDanhById(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM diem_danh WHERE id_hoc_vien = @Id";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new DiemDanhModel // Trả về 1 DiemDanhModel
+                            {
+                                id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]),
+                                id_buoi_hoc = Convert.ToInt32(reader["id_buoi_hoc"]),
+                                ghi_chu = Convert.ToString(reader["ghi_chu"]),
+                                co_mat = Convert.ToInt32(reader["co_mat"])
+                            };
+                        }
+                        else
+                            return null; // Không có trả về null
+                    }
+                }
+            }
+        }
+
+        // Trả về Response
+        public Response InsertDiemdanh(DiemDanhModel diemDanh)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO diemDanh (id_buoi_hoc, ghi_chu, co_mat) " +
+                                   "VALUES (@id_buoi_hoc, @ghi_chu, @co_mat); SELECT LAST_INSERT_ID();";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_buoi_hoc", diemDanh.id_buoi_hoc);
+                        command.Parameters.AddWithValue("@ghi_chu", diemDanh.ghi_chu);
+                        command.Parameters.AddWithValue("@co_mat", diemDanh.co_mat);
+
+                        int insertedId = Convert.ToInt32(command.ExecuteScalar());
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Thêm thông tin điểm danh thành công",
+                            InsertedId = insertedId,
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response UpdateDiemDanh(DiemDanhModel diemDanh)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "UPDATE diem_danh SET id_buoi_hoc = @id_buoi_hoc, " +
+                                   "ghi_chu = @ghi_chu, co_mat = @co_mat " +
+                    "WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_buoi_hoc", diemDanh.id_buoi_hoc);
+                        command.Parameters.AddWithValue("@ghi_chu", diemDanh.ghi_chu);
+                        command.Parameters.AddWithValue("@co_mat", diemDanh.co_mat);
+                        command.Parameters.AddWithValue("@Id", diemDanh.id_hoc_vien);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin điểm danh thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response DeleteDiemDanh(int id)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM diem_danh WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin điểm danh thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Models/HocVien.cs
@@ -0,0 +1,213 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CourseWebsiteDotNet.Models
+{
+    // Lớp HocVienModel chứa các thuộc tính
+    public class HocVienModel
+    {
+        public int id_hoc_vien { get; set; }
+        public string ho_ten { get; set; }
+        public DateTime? ngay_sinh { get; set; }
+        public int? gioi_tinh { get; set; }
+        public string email { get; set; }
+    }
+
+
+    // Lớp HocVienRepository chứa các hàm thao tác với cơ sở dữ liệu
+    public class HocVienRepository
+    {
+        private readonly string connectionString;
+
+        public HocVienRepository()
+        {
+            connectionString = DatabaseConnection.CONNECTION_STRING;
+        }
+
+        // Hàm try catch lỗi từ database
+        private Response ExecuteDatabaseOperation(Func<Response> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (MySqlException dbEx)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Database Exception: {dbEx.Message}",
+                    InsertedId = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Exception: {ex.Message}",
+                    InsertedId = null
+                };
+            }
+        }
+
+        // Trả về List<HocVienModel>
+        public List<HocVienModel> GetAllHocVien()
+        {
+            List<HocVienModel> hocVienList = new List<HocVienModel>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM hoc_vien";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            HocVienModel hocVien = new HocVienModel();
+                            hocVien.id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]);
+                            hocVien.ho_ten = Convert.ToString(reader["ho_ten"]);
+                            hocVien.ngay_sinh = Convert.ToDateTime(reader["ngay_sinh"]);
+                            hocVien.gioi_tinh = Convert.ToInt32(reader["gioi_tinh"]);
+                            hocVien.email = Convert.ToString(reader["email"]);
+                            hocVienList.Add(hocVien);
+                        }
+                    }
+                }
+            }
+
+            return hocVienList;
+        }
+
+        // Trả về 1 HocVienModel
+        public HocVienModel? GetHocVienById(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM hoc_vien WHERE id_hoc_vien = @Id";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new HocVienModel // Trả về 1 HocVienModel
+                            {
+                                id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]),
+                                ho_ten = Convert.ToString(reader["ho_ten"]),
+                                ngay_sinh = Convert.ToDateTime(reader["ngay_sinh"]),
+                                gioi_tinh = Convert.ToInt32(reader["gioi_tinh"]),
+                                email = Convert.ToString(reader["email"]),
+                            };
+                        }
+                        else
+                            return null; // Không có trả về null
+                    }
+                }
+            }
+        }
+
+        // Trả về Response
+        public Response InsertHocVien(HocVienModel hocVien)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO hoc_vien (ho_ten, ngay_sinh, gioi_tinh, email) " +
+                                   "VALUES (@ho_ten, @ngay_sinh, @gioi_tinh, @email); SELECT LAST_INSERT_ID();";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ho_ten", hocVien.ho_ten);
+                        command.Parameters.AddWithValue("@ngay_sinh", hocVien.ngay_sinh);
+                        command.Parameters.AddWithValue("@gioi_tinh", hocVien.gioi_tinh);
+                        command.Parameters.AddWithValue("@email", hocVien.email);
+
+                        int insertedId = Convert.ToInt32(command.ExecuteScalar());
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Thêm học viên thành công",
+                            InsertedId = insertedId,
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response UpdateHocVien(HocVienModel hocVien)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "UPDATE hoc_vien SET ho_ten = @ho_ten, ngay_sinh = @ngay_sinh, " +
+                                   "gioi_tinh = @gioi_tinh, email = @email " +
+                    "WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ho_ten", hocVien.ho_ten);
+                        command.Parameters.AddWithValue("@ngay_sinh", hocVien.ngay_sinh);
+                        command.Parameters.AddWithValue("@gioi_tinh", hocVien.gioi_tinh);
+                        command.Parameters.AddWithValue("@email", hocVien.email);
+                        command.Parameters.AddWithValue("@Id", hocVien.id_hoc_vien);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin học viên thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response DeleteHocVien(int id)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM hoc_vien WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin học viên thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+    }
+}
