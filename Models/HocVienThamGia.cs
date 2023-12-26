9633aa03b9bde1bdba42b2a5ef05a76292a1b559
sunsun1102 <sonhoang880@gmail.com>
2023-12-26 11:03:27 +0700

    Model Update

---
+++ Models/HocVienThamGia.cs
@@ -0,0 +1,196 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CourseWebsiteDotNet.Models
+{
+    // Lớp HocVienThamGiaModel chứa các thuộc tính
+    public class HocVienThamGiaModel
+    {
+        public int id_hoc_vien { get; set; }
+        public int id_lop_hoc { get; set; }
+    }
+
+
+    // Lớp HocVienThamGiaRepository chứa các hàm thao tác với cơ sở dữ liệu
+    public class HocVienThamGiaRepository
+    {
+        private readonly string connectionString;
+
+        public HocVienThamGiaRepository()
+        {
+            connectionString = DatabaseConnection.CONNECTION_STRING;
+        }
+
+        // Hàm try catch lỗi từ database
+        private Response ExecuteDatabaseOperation(Func<Response> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (MySqlException dbEx)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Database Exception: {dbEx.Message}",
+                    InsertedId = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Exception: {ex.Message}",
+                    InsertedId = null
+                };
+            }
+        }
+
+        // Trả về List<HocVienThamGiaModel>
+        public List<HocVienThamGiaModel> GetAllHocVienThamGia()
+        {
+            List<HocVienThamGiaModel> hocVienThamGiaList = new List<HocVienThamGiaModel>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM hoc_vien_tham_gia";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            HocVienThamGiaModel hocVienThamGia = new HocVienThamGiaModel();
+                            hocVienThamGia.id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]);
+                            hocVienThamGia.id_lop_hoc = Convert.ToInt32(reader["id_lop_hoc"]);
+                            hocVienThamGiaList.Add(hocVienThamGia);
+                        }
+                    }
+                }
+            }
+
+            return hocVienThamGiaList;
+        }
+
+        // Trả về 1 HocVienThamGiaModel
+        public HocVienThamGiaModel? GetHocVienThamGiaById(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM hoc_vien_tham_gia WHERE id_hoc_vien = @Id";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new HocVienThamGiaModel // Trả về 1 HocVienThamGiaModel
+                            {
+                                id_hoc_vien = Convert.ToInt32(reader["id_hoc_vien"]),
+                                id_lop_hoc = Convert.ToInt32(reader["id_lop_hoc"])
+                            };
+                        }
+                        else
+                            return null; // Không có trả về null
+                    }
+                }
+            }
+        }
+
+        // Trả về Response
+        public Response InsertHocVienThamGia(HocVienThamGiaModel hocVienThamGia)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO hoc_vien_tham_gia (id_lop_hoc) " +
+                                   "VALUES (@id_lop_hoc); SELECT LAST_INSERT_ID();";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_lop_hoc", hocVienThamGia.id_lop_hoc);
+
+                        int insertedId = Convert.ToInt32(command.ExecuteScalar());
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Thêm thông tin học viên tham gia thành công",
+                            InsertedId = insertedId,
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response UpdateHocVienThamGia(HocVienThamGiaModel hocVienThamGia)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "UPDATE hoc_vien_tham_gia SET id_lop_hoc = @id_lop_hoc WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_lop_hoc", hocVienThamGia.id_lop_hoc);
+                        command.Parameters.AddWithValue("@Id", hocVienThamGia.id_hoc_vien);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin học viên tham gia thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response DeleteHocVienThamGia(int id)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM hoc_vien_tham_gia WHERE id_hoc_vien = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin học viên tham gia thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Models/Muc.cs
@@ -0,0 +1,208 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CourseWebsiteDotNet.Models
+{
+    // Lớp MucModel chứa các thuộc tính
+    public class MucModel
+    {
+        public int id_muc { get; set; }
+        public string? ten_muc { get; set; }
+        public int id_lop_hoc { get; set; }
+        public int? id_muc_cha { get; set; }
+    }
+
+
+    // Lớp MucRepository chứa các hàm thao tác với cơ sở dữ liệu
+    public class MucRepository
+    {
+        private readonly string connectionString;
+
+        public MucRepository()
+        {
+            connectionString = DatabaseConnection.CONNECTION_STRING;
+        }
+
+        // Hàm try catch lỗi từ database
+        private Response ExecuteDatabaseOperation(Func<Response> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (MySqlException dbEx)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Database Exception: {dbEx.Message}",
+                    InsertedId = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Exception: {ex.Message}",
+                    InsertedId = null
+                };
+            }
+        }
+
+        // Trả về List<MucModel>
+        public List<MucModel> GetAllMuc()
+        {
+            List<MucModel> mucList = new List<MucModel>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM muc";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MucModel muc = new MucModel();
+                            muc.id_muc = Convert.ToInt32(reader["id_muc"]);
+                            muc.ten_muc = (reader["ten_muc"]) != DBNull.Value ? Convert.ToString(reader["ten_muc"]) : (string?)null;
+                            muc.id_lop_hoc = Convert.ToInt32(reader["id_lop_hoc"]);
+                            muc.id_muc_cha = (reader["id_muc_cha"]) != DBNull.Value ? Convert.ToInt32(reader["id_muc_cha"]) : (int?)null;
+                            mucList.Add(muc);
+                        }
+                    }
+                }
+            }
+
+            return mucList;
+        }
+
+        // Trả về 1 MucModel
+        public MucModel? GetMucById(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM muc WHERE id_muc = @Id";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new MucModel // Trả về 1 MucModel
+                            {
+                                id_muc = Convert.ToInt32(reader["id_muc"]),
+                                ten_muc = (reader["ten_muc"]) != DBNull.Value ? Convert.ToString(reader["ten_muc"]) : (string?)null,
+                                id_lop_hoc = Convert.ToInt32(reader["id_lop_hoc"]),
+                                id_muc_cha = (reader["id_muc_cha"]) != DBNull.Value ? Convert.ToInt32(reader["id_muc_cha"]) : (int?)null
+                            };
+                        }
+                        else
+                            return null; // Không có trả về null
+                    }
+                }
+            }
+        }
+
+        // Trả về Response
+        public Response InsertMuc(MucModel muc)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO muc (ten_muc, id_lop_hoc, id_muc_cha) " +
+                                   "VALUES (@ten_muc, @id_lop_hoc, @id_muc_cha); SELECT LAST_INSERT_ID();";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ten_muc", muc.ten_muc);
+                        command.Parameters.AddWithValue("@id_lop_hoc", muc.id_muc);
+                        command.Parameters.AddWithValue("@id_muc_cha", muc.id_muc_cha);
+
+                        int insertedId = Convert.ToInt32(command.ExecuteScalar());
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Thêm mục thành công",
+                            InsertedId = insertedId,
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response UpdateMuc(MucModel muc)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "UPDATE muc SET ten_muc = @ten_muc, " +
+                                   "id_lop_hoc = @id_lop_hoc, id_muc_cha = @id_muc_cha " +
+                    "WHERE id_muc = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ten_muc", muc.ten_muc);
+                        command.Parameters.AddWithValue("@id_lop_hoc", muc.id_lop_hoc);
+                        command.Parameters.AddWithValue("@id_muc_cha", muc.id_muc_cha);
+                        command.Parameters.AddWithValue("@Id", muc.id_muc);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin mục thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+
+        // Trả về Response
+        public Response DeleteMuc(int id)
+        {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM muc WHERE id_muc = @Id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Cập nhật thông tin mục thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+    }
+}
