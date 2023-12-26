c55b9816871b766949c7889c6e1c8c3705cc9946
sunsun1102 <sonhoang880@gmail.com>
2023-12-26 10:09:43 +0700

    Untrack files specified in .gitignore

--- Models/DatabaseConnection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CourseWebsiteDotNet.Models
-{
-    public class DatabaseConnection
-    {
-        public static readonly string CONNECTION_STRING = "Server=localhost;Database=coursesdb;Uid=root;Pwd=";
-    }
-}
