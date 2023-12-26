bffd133d6d972ca69c9db0cee4deee77f96e2d5f
sunsun1102 <sonhoang880@gmail.com>
2023-12-26 10:22:42 +0700

    Add all

---
+++ Models/DatabaseConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace CourseWebsiteDotNet.Models
+{
+    public class DatabaseConnection
+    {
+        public static readonly string CONNECTION_STRING = "Server=localhost;Database=coursesdb;Uid=root;Pwd=";
+    }
+}
---
+++ Models/SQLExecutor.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace CourseWebsiteDotNet.Models
+{
+    public class SQLExecutor
+    {
+        public static DataTable ExecuteQuery(string sql)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (MySqlConnection connection = new MySqlConnection(DatabaseConnection.CONNECTION_STRING))
+            {
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        public static Response ExecuteDML(string Sql) {
+            return ExecuteDatabaseOperation(() =>
+            {
+                using (MySqlConnection connection = new MySqlConnection(DatabaseConnection.CONNECTION_STRING))
+                {
+                    connection.Open();
+
+                    string query = Sql;
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        int effectedRows = command.ExecuteNonQuery();
+
+                        return new Response
+                        {
+                            State = true,
+                            Message = "Thực thi câu lệnh thành công",
+                            InsertedId = null,
+                            EffectedRows = effectedRows
+                        };
+                    }
+                }
+            });
+        }
+
+
+        private static Response ExecuteDatabaseOperation(Func<Response> operation)
+        {
+            try
+            {
+                return operation();
+            }
+            catch (MySqlException dbEx)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Database Exception: {dbEx.Message}",
+                    InsertedId = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    State = false,
+                    Message = $"Exception: {ex.Message}",
+                    InsertedId = null
+                };
+            }
+        }
+    }
+}
