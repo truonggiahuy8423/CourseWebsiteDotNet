7ef5ce6b753c7ea2087a55f81b8914ec3a4ac1a1
PNLamHa <21522030@gm.uit.edu.vn>
2023-12-28 23:17:24 +0700

    ok add quenmatkhau

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -3,6 +3,8 @@ using CourseWebsiteDotNet.Models;
 using Microsoft.AspNetCore.Http;
 using MySqlX.XDevAPI.Common;
 using Newtonsoft.Json;
+using System.Net.Mail;
+using System.Net;
 
 namespace CourseWebsiteDotNet.Controllers
 {
@@ -123,6 +125,139 @@ namespace CourseWebsiteDotNet.Controllers
             }
 
 
+        }
+
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string email)
+        {
+            UserRepository _userRepo = new UserRepository();
+            var user =  _userRepo.GetUserByEmail(email);
+            if (user == null ||  (user.Email == null && user.id_user == null)) return Json(new { result = 0 });
+
+            var codeToken = GenerateRandomCode(6);
+          
+            #region Subject and body
+            var subject = "Reset your password";
+     
+            var body = $@"
+                            <html>
+                            <head>
+                                <style>
+                                    body {{
+                                        font-family: Arial, sans-serif;
+                                        margin: 0;
+                                        padding: 0;
+                                    }}
+                                    .container {{
+                                        max-width: 600px;
+                                        margin: 0 auto;
+                                        padding: 20px;
+                                        background-color: #f7f7f7;
+                                    }}
+                                    .header {{
+                                        background-color: #3498db;
+                                        color: #fff;
+                                        text-align: center;
+                                        padding: 10px 0;
+                                    }}
+                                    .content {{
+                                        background-color: #fff;
+                                        padding: 20px;
+                                        border-radius: 5px;
+                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                    }}
+                                    .footer {{
+                                        text-align: center;
+                                        margin-top: 20px;
+                                        color: #888;
+                                    }}
+                                </style>
+                            </head>
+                            <body>
+                                <div class='container'>
+                                    <div class='header'>
+                                        <h1>Đặt lại mật khẩu của bạn!</h1>
+                                    </div>
+                                    <div class='content'>
+                                        <p>Xin chào !</p>
+                                        <p> Chúng tôi đã nhận được thông tin của bạn về việc quên mật khẩu..</p>
+                                        <p>Chúng tôi đã hỗ trợ đổi mật khẩu:</p>
+                                        <ul>   
+                                            <li><strong>UserName:</strong> {user.tai_khoan}</li>
+                                            <li><strong>New password:</strong> {codeToken}</li>
+                                        </ul>
+                                        <p>Vui lòng liên hệ với chúng tôi nếu việc đổi mật khẩu không thành công..</p>
+                                        <p>Xin cảm ơn và chúc một ngày tốt lành!</p>
+                                    </div>
+                                    <div class='footer'>
+                                        <p>© 2023 Your Company. All rights reserved.</p>
+                                    </div>
+                                </div>
+                            </body>
+                            </html>";
+            #endregion
+
+            if (SendMail(email, subject, body, user.id_user, codeToken))
+            {
+
+
+                return Json(new { result = 1 });//Gửi mail thành công
+            }
+            else
+            {
+                return Json(new { result = 2 });//Gửi không thành công
+            }
+        }
+        private string GenerateRandomCode(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public bool SendMail(string to, string subject, string body,int? userid, string passnew)
+        {
+            try
+            {
+                string fromEmail = "cateringmanagement2023@gmail.com";
+                string password = "hqlo uqlv qjmb wzzm";
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(fromEmail);
+                message.Subject = subject;
+                message.To.Add(new MailAddress(to));
+                message.Body = body;
+                message.IsBodyHtml = true;
+                var smtpClient = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    Credentials = new NetworkCredential(fromEmail, password),
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                };
+                smtpClient.Send(message);
+                UserRepository _userRepo = new UserRepository();
+
+                var  user =  _userRepo.GetUserById(userid.Value);
+
+                if (user != null)
+                {
+                    user.mat_khau = passnew;
+
+                }
+                else
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
